c6b0a2beac78cf1456ee3b32d882a482e53e1817
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2024-01-10 14:52:07 +0100

    något

---
+++ Assets/Codes/ScreenBoundaries.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public float speed;
+    
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.y >= 4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.5f, 0);
+        }
+        else if (transform.position.y <= -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+        else if (transform.position.x <= -10.1f)
+        {
+            transform.position = new Vector3(-10.1f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -10.1f)
+        {
+            transform.position = new Vector3(-10.1f, transform.position.y, 0);
+        }
+            
+        transform.Translate(Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
+
+        transform.Translate(Vector3.up * Time.deltaTime * speed * Input.GetAxis("Vertical"));
+    }
+}
