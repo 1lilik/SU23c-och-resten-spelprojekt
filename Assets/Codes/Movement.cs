d0a3208daae1a6a5d5bfa12a52bc702160cac5dd
lightningmcqueen69 <emily.figueroa@elev.ga.lbs.se>
2024-01-18 19:33:59 +0100

    ur ur ur

--- Assets/Codes/Movement.cs
+++ Assets/Codes/Movement.cs
@@ -4,17 +4,6 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-<<<<<<< HEAD
-=======
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = new Vector3(0, -1, 0);
-
-    }
-
-    // Update is called once per frame
->>>>>>> f28b1fe6188bb3be9d09d80d47c10061b10cec10
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
