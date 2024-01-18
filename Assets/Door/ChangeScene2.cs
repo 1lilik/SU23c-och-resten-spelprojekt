13a289749403a51a411c7c753ce51c13d66a3448
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2024-01-18 16:07:36 +0100

    SceneChange

---
+++ Assets/Door/ChangeScene2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene2 : MonoBehaviour
+{
+    FadeInOut fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = FindObjectOfType<FadeInOut>();
+    }
+
+    public IEnumerator LChangeScene()
+    {
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Gemensam scen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(LChangeScene());
+        }
+    }
+}
